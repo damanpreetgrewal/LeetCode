dfe1a9c65f5e442a06fbe6b5e5de433139a7e2ab
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-08 18:11:43 -0400

    Roman To Integer

---
+++ Day4/Roman_to_Integer_simple.js
@@ -0,0 +1,27 @@
+var romanToInt = function (s) {
+  const numbers = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (numbers[s[i + 1]] && numbers[s[i]] < numbers[s[i + 1]]) {
+      result += -numbers[s[i]];
+    } else {
+      result += numbers[s[i]];
+    }
+  }
+
+  return result;
+};
+
+
+//1767
+console.log(romanToInt("MDCCLXVII"));
