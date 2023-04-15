9a21f43676a0cb5500b658edcf13a6ba81fd72fc
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-15 00:35:17 -0400

    Search Questions on leetcode using File Name

--- Day4/find-the-index-of-the-first-occurrence-in-a-strin.js
+++ Day4/find-the-index-of-the-first-occurrence-in-a-strin.js
@@ -4,9 +4,24 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-
-
-    
+  if (needle === '') {
+    return 0;
+  }
+  if (haystack === null || needle === null) {
+    return -1;
+  }
+  for (var i = 0; i <= haystack.length; i++) {
+    // Avoid looping needle if first letter doesn't match
+    if (haystack.charAt(i) === needle.charAt(0)) {
+      for (var j = 1; j < needle.length; j++) {
+        if (haystack.charAt(i + j) !== needle.charAt(j)) {
+          continue;
+        }
+      }
+      return i;
+    }
+  }
+  return -1;
 };
 
 console.log(strStr('sadbutsad', 'sad'));
