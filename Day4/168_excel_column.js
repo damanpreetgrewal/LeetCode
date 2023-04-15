b37d1d30c83412077764d31aab800a41cc57dad1
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-14 22:04:12 -0400

    Search Questions on leetcode using File Name

--- Day4/168_excel_column.js
+++ Day4/168_excel_column.js
@@ -2,12 +2,22 @@
  * @param {number} columnNumber
  * @return {string}
  */
-var convertToTitle = function (columnNumber) {
-  let output = 0;
-  let length = columnNumber.length;
-  console.log(length);
-
-  console.log(columnNumber[length - 1].charCodeAt());
+var titleToNumber = function (s) {
+  var res = 0;
+  var num = 0;
+  var len = s.length;
+  for (var i = 0; i < len; i++) {
+    num = getNum(s[len - 1 - i]);
+    console.log('num', num);
+    res += Math.pow(26, i) * num;
+  }
+  return res;
 };
 
-console.log(convertToTitle('AA'));
+var getNum = function (char) {
+  console.log('char', char);
+  var start = 'A'.charCodeAt(0) - 1;
+  return char.charCodeAt(0) - start;
+};
+
+console.log(titleToNumber('ABC'));
