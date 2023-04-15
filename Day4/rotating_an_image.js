97856dbb37b27effbedf78051941d3af4b5ade28
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-15 16:38:44 -0400

    Search Questions on leetcode using File Name

--- Day4/rotating_an_image.js
+++ Day4/rotating_an_image.js
@@ -2,8 +2,24 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function (matrix) {
-    
+const rotate = matrix => {
+  let n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let tmp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tmp;
+      if (i == j) {
+        break;
+      }
+    }
+  }
+
+  for (let k = 0; k < n; k++) {
+    matrix[k].reverse();
+  }
+  return matrix;
 };
 
 console.log(
